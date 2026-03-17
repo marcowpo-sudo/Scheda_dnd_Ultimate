ab1c40f391ac07ac79368ee6ca13183160b5f268
marcowpo-sudo <marcowpo@gmail.com>
2026-03-18 00:31:51 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'dnd-sheet-v1';
+const CACHE_NAME = 'dnd-sheet-v2';
 const urlsToCache = [
   './',
   './index.html',
+  './style.css',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
@@ -12,8 +13,25 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Cache aperta');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+// Attiva il Service Worker e pulisce le vecchie cache (fondamentale per gli aggiornamenti)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Elimino vecchia cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
